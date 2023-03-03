ee22b4529ba6dd689a87c3b5654efaa3a88e6653
DenisShytikov <denchik.shitikov@gmail.com>
2023-03-03 12:47:02 +0200

    Add file

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+devices = "DoorProtect DoorProtect MotionProtect MotionProtect FireProtect FireProtectPlus FireProtect DoorProtect"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+devices_list = devices.split()
+device_dict = {}
 
+for device in devices_list:
+     if device not in device_dict:
+        device_dict[device] = 1
+     else:
+        device_dict[device] = device_dict[device] + 1
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print(device_dict)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
